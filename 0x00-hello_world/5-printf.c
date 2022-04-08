9b8be0bf518bb840ac1ffa75ba4eb8f2b526de13
franklin <franklinmuoneke02@gmail.com>
2022-04-08 12:36:39 -0700

    6th task commit for c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
-#include <stdio.h>
+#include <stdio>
+
 /**
- *main - Entry point
- *My hello, printf task
- *Return: return 1 after program execution
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-printf("with proper grammar, but the output is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,â—\n");
 return (0);
+}
