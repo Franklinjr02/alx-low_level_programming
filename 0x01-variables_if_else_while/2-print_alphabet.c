e0cdf3ee20d6973cf74a1dc2f55112f393a9e45e
franklin <franklinmuoneke02@gmail.com>
2022-04-16 00:22:23 -0700

    3rd task for operations

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,15 @@
 #include <stdio.h>
 /**
- * main -Entry point
+ * main - Entry point
  *
- *Return: Always 0 (Sucess/correct)
+ * Return: Always 0 (Success)
  */
-int main (void)
-
+int main(void)
 {
-char alphabet;
-for (alphabet = 'a'; alphabet (= 'z'; alphabet++)
-{
-putchar(alphabet);
-}
+char alpha;
 
-putchar('/n');
-
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+putchar(alpha);
+putchar('\n');
 return (0);
-
 }
