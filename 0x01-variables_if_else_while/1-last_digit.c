b600a316302ede0fdb1d627f8d7d6b61325aa3e1
franklin <franklinmuoneke02@gmail.com>
2022-04-15 23:57:55 -0700

    my second task for operations

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,31 +1,31 @@
-GNU nano 4.8               0-positive_or_negative.c
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 /**
- * main - Entry Point
+ * main - Entry point
  *
- * Return: Always 0 (Success/correct)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 int n;
+int ld = 0;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
-If (n % 10 > 5)
+ld = n % 10;
+if (ld == 0)
 {
-printf("Last digit of %i is %i amd is greater than 5\n", n, n % 10);
+printf("Last digit of %i is %i and is 0\n", n, ld);
 }
-
-else if (n % 10 == 0)
+else if (ld < 6 && ld != 0)
 {
-printf("Last digit of %i is %i and is 0\n", n, n % 10);
+printf("Last digit of %i is %i and is less than 6 and not 0\n", n, ld);
 }
-
-else
+if (ld > 5)
 {
-printf("Last digit of %i is %i and is less than 6 and not 0\n", n, n % 10);
+printf("Last digit of %i is %i and is greater than 5\n", n, ld);
 }
-
 return (0);
 }
