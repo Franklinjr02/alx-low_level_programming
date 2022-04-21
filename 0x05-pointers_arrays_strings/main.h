f910ead90914c24757a51679433e2b76b8b24469
root <franklinmuoneke02@gmail.com>
2022-04-21 06:48:36 -0700

    1st task commit

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,22 +1,24 @@
-int _putchar(char c);
+#ifndef MAIN_H
+#define MAIN_H
 
-void swap_int(int *a, int *b);
+/**
+* Description: header file containibg prototypes for all functions
+* used in the 0x05-pointers_arrays_strings 
+*/
 
-int _strlen(char *s);
+#include <stdio.h>
 
-void _puts(char *str);
+int _putchar(char c)
+void reset_to_98(int *n);
+void​ ​swap_int​(​int​ *a, ​int​ *b); 
+ ​int​ ​_strlen​(​char​ *s); 
+ ​void​ ​_puts​(​char​ *str); 
+ ​void​ ​print_rev​(​char​ *s); 
+ ​void​ ​rev_string​(​char​ *s); 
+ ​void​ ​puts2​(​char​ *str); 
+ ​void​ ​puts_half​(​char​ *str); 
+ ​void​ ​print_array​(​int​ *a, ​int​ n); 
+ ​char​ *​_strcpy​(​char​ *dest, ​char​ *src); 
+ ​int​ ​_atoi​(​char​ *s);
 
-void print_rev(char *s);
-
-void rev_string(char *s);
-
-void puts2(char *str);
-
-void puts_half(char *str);
-
-void print_array(int *a, int n);
-
-char *_strcpy(char *dest, char *src);
-
-int _atoi(char *s);
-
+#endif
