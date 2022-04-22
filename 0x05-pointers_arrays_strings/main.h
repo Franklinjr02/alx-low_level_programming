359bda53e98dd7bc476e1aa0b2fa592a98d51e4b
root <franklinmuoneke02@gmail.com>
2022-04-21 17:44:24 -0700

    1st task for c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,24 +1,22 @@
-#ifndef MAIN_H
-#define MAIN_H
+int _putchar(char c);
 
-/**
-* Description: header file containibg prototypes for all functions
-* used in the 0x05-pointers_arrays_strings 
-*/
+void swap_int(int *a, int *b);
 
-#include <stdio.h>
+int _strlen(char *s);
 
-int _putchar(char c)
-void reset_to_98(int *n);
-void​ ​swap_int​(​int​ *a, ​int​ *b); 
- ​int​ ​_strlen​(​char​ *s); 
- ​void​ ​_puts​(​char​ *str); 
- ​void​ ​print_rev​(​char​ *s); 
- ​void​ ​rev_string​(​char​ *s); 
- ​void​ ​puts2​(​char​ *str); 
- ​void​ ​puts_half​(​char​ *str); 
- ​void​ ​print_array​(​int​ *a, ​int​ n); 
- ​char​ *​_strcpy​(​char​ *dest, ​char​ *src); 
- ​int​ ​_atoi​(​char​ *s);
+void _puts(char *str);
 
-#endif
+void print_rev(char *s);
+
+void rev_string(char *s);
+
+void puts2(char *str);
+
+void puts_half(char *str);
+
+void print_array(int *a, int n);
+
+char *_strcpy(char *dest, char *src);
+
+int _atoi(char *s);
+
