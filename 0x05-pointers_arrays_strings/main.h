1ae7745bd324180e8191a9eafed40f7296151c4e
root <franklinmuoneke02@gmail.com>
2022-04-21 18:31:31 -0700

    1st task

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,16 @@
+#ifndef MAIN_H
+
+#define MAIN_H
+
+
+
+#include <stdio.h>
+
+
+
 int _putchar(char c);
+
+void reset_to_98(int *n);
 
 void swap_int(int *a, int *b);
 
@@ -20,3 +32,6 @@ char *_strcpy(char *dest, char *src);
 
 int _atoi(char *s);
 
+
+
+#endif /* MAIN.H */
