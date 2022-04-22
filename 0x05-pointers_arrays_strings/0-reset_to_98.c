9d4839add2a12198c62a4a3ee49ce53a9eb48f3b
Franklinjr02 <101658484+Franklinjr02@users.noreply.github.com>
2022-04-21 18:48:54 -0700

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,30 +1,11 @@
-#​include​ ​"​main.h​" 
- ​/*​* 
- ​ * reset_to_98 - reset argument to 98 
- ​ * @n: pointer 
- ​ * Return: void 
- ​*/ 
-  
- ​void​ ​reset_to_98​(​int​ *n) 
- ​{ 
-  
- ​        *n = ​98​; 
-  
- ​}
+#include "main.h"
 
-task 1: 1-swap.c
- 
- ​#​include​ ​"​main.h​" 
- ​/*​* 
- ​ * swap_int - swap variable values 
- ​ * @a: pointer 1 
- ​ * @b: pointer 2 
- ​ * Return: void 
- ​*/ 
-  
- ​void​ ​swap_int​(​int​ *a, ​int​ *b) 
- ​{ 
- ​        *a += *b; 
- ​        *b = *a - *b; 
- ​        *a = *a - *b; 
- ​}
+/**
+ * reset_to_98 - change the value of integer using pointer
+ * @n: pointer
+ * Return:void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
